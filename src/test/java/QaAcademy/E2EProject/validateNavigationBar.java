3e95ad90b7a3831120f0ba0a12245a6fcebab07c
Ashok <ashokspeedy@gmail.com>
2022-01-11 08:04:47 +0530

    AshokRep added Test3 in develop validateNavigationBar

--- src/test/java/QaAcademy/E2EProject/validateNavigationBar.java
+++ src/test/java/QaAcademy/E2EProject/validateNavigationBar.java
@@ -50,6 +50,14 @@ public class validateNavigationBar extends base{
 		
 	}
 	
+	@Test
+	public void navigationBar3() throws IOException {
+		LandingPage lp=new LandingPage(driver);
+		Assert.assertTrue(lp.contact().isDisplayed());
+		log.info("Element is displayed successfully");
+		
+	}
+	
 	@AfterTest
 	public void closeBrowser() {
 		driver.close();
